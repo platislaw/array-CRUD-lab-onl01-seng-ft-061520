c5c8f62e741f691de916879bd88544e6c80fff45
Paul Latislaw <paullatislaw@gmail.com>
2020-06-18 00:42:14 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,9 @@ end
 
 def add_element_to_end_of_array(array, element)
  array = [1,2,3,4]
- array << 5
+ element = 5
+ array.push(element)
+
 end
 
 def add_element_to_start_of_array(array, element)
