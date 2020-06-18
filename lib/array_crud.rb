d099eba111181c82eaf57534eeb5ceb0127862d3
Paul Latislaw <paullatislaw@gmail.com>
2020-06-18 00:45:55 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -19,7 +19,9 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
-
+  array = [1,2,3,4]
+  element =  "arrays"
+  array.pop(element)
 end
 
 def remove_element_from_start_of_array(array)
